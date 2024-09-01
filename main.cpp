040f9156261dbc78b4ab2b5a253542b92d547db4
web3wesrecruiting <web3wesrecruiting@gmail.com>
2024-09-01 00:11:57 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
+#include <string>
 
-int main() { std::cout << "Hello World!\n"; }
+int main() {
+    std::string firstName;
+    
+    std::cout << "Please enter your first name: ";
+    std::cin >> firstName;
+    
+    std::cout << "Hello, " << firstName << "! Welcome to the world of C++!\n";
+    
+    return 0;
+}
